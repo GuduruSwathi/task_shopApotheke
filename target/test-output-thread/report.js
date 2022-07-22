0e37bcefe78e869561a9db85f56c57562bae5fc0
Swathi Guduru <guduru.swathii@gmail.com>
2022-07-22 15:20:46 +0200

    Task

---
+++ target/test-output-thread/report.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "71177039-06fc-4b90-841c-ad1668d8cd3c",
+    "feature": "Login tests",
+    "scenario": "Invalid Login",
+    "start": 1658494618656,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1658494858233,
+    "className": "failed"
+  },
+  {
+    "id": "b8f52915-62bb-41be-add5-cf20878b6a70",
+    "feature": "Login tests",
+    "scenario": "Invalid Login",
+    "start": 1658494858255,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1658495022157,
+    "className": "failed"
+  },
+  {
+    "id": "1913ea70-0c89-4979-8331-3239c99dfc2a",
+    "feature": "Login tests",
+    "scenario": "Valid Login",
+    "start": 1658493599553,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1658494618587,
+    "className": "failed"
+  },
+  {
+    "id": "8d466ec5-9b7e-4127-a658-b0ce7c0108b2",
+    "feature": "Login tests",
+    "scenario": "Change Password",
+    "start": 1658495022167,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1658495028777,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
